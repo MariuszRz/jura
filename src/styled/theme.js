1ad034d9c8428eab0bc258dae5bd46373ba02229
Mariusz Rzadkiewicz <mariusz.rz@wp.pl>
2021-08-17 09:54:23 +0200

    add tyled-component and theme

---
+++ src/styled/theme.js
@@ -0,0 +1,14 @@
+export default {
+    spacing: {
+        xs: 5,
+        sm: 10,
+        xl: 20
+    },
+    color: {
+        main: {
+            light: ' Light',
+            normal: '#37474f',
+            dark: '#102027',
+        }
+    }
+}
